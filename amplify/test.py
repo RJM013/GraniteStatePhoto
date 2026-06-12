762f0e8bef847a8ec216fa40e781bde6b3365d37
RJMulligan <86175047+RJM013@users.noreply.github.com>
2026-06-11 20:40:07 -0400

    form

---
+++ amplify/test.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+
+x = [1, 2, 3, 4, 5]
+
+
+for i in x:
+    print(i)
+    print("Hello, World!")
+    print("Hello, World!")
+    print("Hello, World!")
+    print("Hello, World!")
